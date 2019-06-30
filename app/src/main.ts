6314b162a8de4303b8becf0ab2bb1192051aa376
Henning Berge <henning.ber@gmail.com>
2019-06-30 14:56:31 +0200

    mitigate bug where it would crash after selecting the map

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,23 +4,27 @@ import { Trigger } from './JassOverrides/Trigger';
 import { Log, LogLevel } from './lib/Serilog/Serilog';
 import { StringSink } from './lib/Serilog/Sinks/StringSink';
 
-Log.Init([
-    new StringSink(LogLevel.Fatal, SendMessage),
-]);
+
+ceres.addHook('main::after', () => {
+    Log.Init([
+        new StringSink(LogLevel.Fatal, SendMessage),
+    ]);
 
 
-function Main(this: void): void {
-    const maul: WarcraftMaul = new WarcraftMaul();
-    if (maul.debugMode) {
-        Log.Information('Initialisation finished');
+    function Main(this: void): void {
+        const maul: WarcraftMaul = new WarcraftMaul();
+        if (maul.debugMode) {
+            Log.Information('Initialisation finished');
+        }
     }
-}
 
-xpcall(() => {
-    const init: Trigger = new Trigger();
-    init.RegisterTimerEvent(0.00, false);
-    init.AddAction(() => Main());
-},     (err) => {
-    Log.Fatal(err);
+    xpcall(() => {
+        const init: Trigger = new Trigger();
+        init.RegisterTimerEvent(0.00, false);
+        init.AddAction(() => Main());
+    },     (err) => {
+        Log.Fatal(err);
+    });
+
 });
 
