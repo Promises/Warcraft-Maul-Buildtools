1c65641b9760d56962619d684ef273f76f65f4d0
Henning Berge <henning.ber@gmail.com>
2019-07-01 17:35:17 +0200

    test prettify game time and player size

--- app/src/main_spec.ts
+++ app/src/main_spec.ts
@@ -5,6 +5,7 @@ import '../spec/ceresmocking';
 
 import { WarcraftMaul } from './World/WarcraftMaul';
 import { StringSinkTest } from './lib/Serilog/Sinks/StringSinkTest';
+import * as settings from './World/GlobalSettings';
 
 Log.Init([
     new StringSinkTest(LogLevel.Fatal, print),
@@ -15,6 +16,8 @@ describe('FourCC', () => {
     it('converts to and from FourCC', () => {
 
         const game: WarcraftMaul = new WarcraftMaul();
+        assert.equals(settings.players.size, 13);
+        assert.equals(game.PrettifyGameTime(1000), '|cFF99999900:16:40|r');
 
     });
 });
