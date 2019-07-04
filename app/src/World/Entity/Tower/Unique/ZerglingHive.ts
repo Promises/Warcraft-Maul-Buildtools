a4ca7b7cd18cd4d4d866eb8fe62811afe8af2d0a
Promises <henning.ber@gmail.com>
2019-07-04 20:55:51 +0200

    Tweak zergling hive to spawn every 35 hits + first of every wave

--- app/src/World/Entity/Tower/Unique/ZerglingHive.ts
+++ app/src/World/Entity/Tower/Unique/ZerglingHive.ts
@@ -1,9 +1,15 @@
 import { Tower } from '../Specs/Tower';
 import { GenericAutoAttackTower } from '../Specs/GenericAutoAttackTower';
+import { EndOfRoundTower } from '../Specs/EndOfRoundTower';
 
-export class ZerglingHive extends Tower implements GenericAutoAttackTower {
+export class ZerglingHive extends Tower implements GenericAutoAttackTower, EndOfRoundTower {
+    private chance: number = 30;
+    private currentHit: number = 30;
+
+
     public GenericAttack(): void {
-        if (Util.RandomInt(1, 10) === 1) {
+        if (this.currentHit >= this.chance) {
+            this.currentHit = 0;
             const tempUnit: unit = CreateUnit(
                 this.owner.wcPlayer,
                 FourCC('u042'),
@@ -12,6 +18,12 @@ export class ZerglingHive extends Tower implements GenericAutoAttackTower {
                 bj_UNIT_FACING);
             UnitApplyTimedLifeBJ(10.00, FourCC('BTLF'), tempUnit);
         }
+        this.currentHit++;
+
+    }
+
+    public EndOfRoundAction(): void {
+        this.currentHit = 30;
     }
 
 }
