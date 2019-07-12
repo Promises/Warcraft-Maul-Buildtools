c1ce84384630dfa51e00176a185d7755d01277d4
Promises <henning.ber@gmail.com>
2019-07-12 19:39:40 +0200

    corrupted ancient protector only during wave

--- app/src/World/Entity/Tower/Races/CorruptedNightElves/CorruptedAncientProtector.ts
+++ app/src/World/Entity/Tower/Races/CorruptedNightElves/CorruptedAncientProtector.ts
@@ -3,12 +3,14 @@ import { TickingTower } from '../../Specs/TickingTower';
 
 export class CorruptedAncientProtector extends Tower implements TickingTower {
     public Action(): void {
-        const x: number = GetUnitX(this.tower);
-        const y: number = GetUnitY(this.tower);
-        const dummy: unit = CreateUnit(this.owner.wcPlayer, FourCC('u008'), x, y, 0);
-        UnitApplyTimedLifeBJ(11.00, FourCC('BTLF'), dummy);
-        UnitAddAbilityBJ(FourCC('A010'), dummy);
-        IssueImmediateOrderBJ(dummy, 'starfall');
+        if (this.game.gameRoundHandler.isWaveInProgress) {
+            const y: number = GetUnitY(this.tower);
+            const x: number = GetUnitX(this.tower);
+            const dummy: unit = CreateUnit(this.owner.wcPlayer, FourCC('u008'), x, y, 0);
+            UnitApplyTimedLifeBJ(11.00, FourCC('BTLF'), dummy);
+            UnitAddAbilityBJ(FourCC('A010'), dummy);
+            IssueImmediateOrderBJ(dummy, 'starfall');
+        }
     }
 
     public GetTickModulo(): number {
