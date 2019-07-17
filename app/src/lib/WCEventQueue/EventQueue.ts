9c8c1446aa3c200be1986abce970255f9c6c5eb5
Promises <henning.ber@gmail.com>
2019-07-17 13:49:52 +0200

    eventqueue does not need logging

--- app/src/lib/WCEventQueue/EventQueue.ts
+++ app/src/lib/WCEventQueue/EventQueue.ts
@@ -22,7 +22,6 @@ export class EventQueue {
     private HandleTick(): void {
         let j = 0;
         if (this.currentTask) {
-            Log.Debug('There is a task');
             for (let i: number = 0; i < this.maxEventsPerTick; i++) {
                 if (this.currentTask) {
                     if (this.currentTask()) {
@@ -41,18 +40,14 @@ export class EventQueue {
 
     private GetTask(): void {
         if (this.highPriority.size() > 0) {
-            Log.Debug('found high');
-
             this.currentTask = this.highPriority.pop();
             return;
         }
         if (this.medPriority.size() > 0) {
-            Log.Debug('found med');
             this.currentTask = this.medPriority.pop();
             return;
         }
         if (this.lowPriority.size() > 0) {
-            Log.Debug('found low');
             this.currentTask = this.lowPriority.pop();
             return;
         }
@@ -63,8 +58,6 @@ export class EventQueue {
     }
 
     public AddMed(event: () => boolean): void {
-        Log.Debug('Added to queue');
-
         this.medPriority.push(event);
     }
 
