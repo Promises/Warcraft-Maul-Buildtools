17ed480259518e9f4e776474fe872c4415d1110b
Henning <henning.ber@gmail.com>
2019-07-31 20:59:51 +0200

    fix hydralisk charges

--- app/src/World/Entity/Tower/Races/Unique/Hydralisk.ts
+++ app/src/World/Entity/Tower/Races/Unique/Hydralisk.ts
@@ -8,9 +8,7 @@ export class Hydralisk extends Tower implements KillingActionTower {
         const u: unit | undefined = GetKillingUnitBJ();
         const target: unit | undefined = GetDyingUnit();
 
-        if (Util.RandomInt(1, 2) === 1) {
-            return;
-        }
+
         if (u === this.tower && target) {
             if (GetUnitAbilityLevelSwapped(FourCC('A0EH'), this.tower) <= 50) {
                 IncUnitAbilityLevelSwapped(FourCC('A0EH'), this.tower);
