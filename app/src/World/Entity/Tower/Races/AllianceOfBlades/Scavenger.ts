2cdd7290390892ff7b784aecd18172cc425d2895
Henning Berge <henning.ber@gmail.com>
2019-08-15 11:35:36 +0200

    stop flooding ground with items

--- app/src/World/Entity/Tower/Races/AllianceOfBlades/Scavenger.ts
+++ app/src/World/Entity/Tower/Races/AllianceOfBlades/Scavenger.ts
@@ -1,15 +1,19 @@
 import { Tower } from '../../Specs/Tower';
 import { AOB_ITEM_LOOT_LEVEL_ONE } from '../../../../GlobalSettings';
 import { TickingTower } from '../../Specs/TickingTower';
+import { Log } from '../../../../../lib/Serilog/Serilog';
 
 export class Scavenger extends Tower implements TickingTower {
 
     public Scavenge(): void {
 
         if (UnitItemInSlot(this.tower, 0) === null) {
+            // Log.Debug(`${this.handleId}`);
             const lootindx: number = Util.RandomInt(0, AOB_ITEM_LOOT_LEVEL_ONE.length - 1);
             const i: item = CreateItem(AOB_ITEM_LOOT_LEVEL_ONE[lootindx], GetUnitX(this.tower), GetUnitY(this.tower));
-            UnitAddItem(this.tower, i);
+            if (!UnitAddItem(this.tower, i)) {
+                RemoveItem(i);
+            }
         }
 
 
